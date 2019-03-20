68ae96f1a9b49dee07c93a27f1e8fef590e97299
stellaama <stellama98@gmail.com>
2019-03-20 15:32:31 -0400

    Added button functions and css styling

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -8,8 +8,17 @@ class App extends Component {
   constructor(){
     super();
     this.state = {
-      loggedIn: false
+      loggedIn: false,
+      feedback:"",
+      toDoList: []
     };
+  }
+
+  componentDidMount(){
+    if (Cookies.get('token')){
+      this.setState({loggedIn: true});
+      this.printToDoList();
+    }
   }
 
   handleLogin(){
@@ -22,12 +31,14 @@ class App extends Component {
         Cookies.set('username', user);
         this.setState({loggedIn: true});
         console.log("ok done logging in")
+        this.printToDoList();
       }
     })
     .catch((error) => {
       console.log(error)
-      console.log("That user doesn't exist")
+      this.setState({feedback: "That user doesn't exist"})
     })
+    this.refs.username.value = "";
   }
 
   handleLogout(){
@@ -36,28 +47,133 @@ class App extends Component {
     Cookies.remove('username')
   }
 
+  handleRegister(){
+    let user = this.refs.username.value;
+    axios.post('https://hunter-todo-api.herokuapp.com/user', { username: user})
+    .then(res => {
+      //LOGGING NEW USER IN
+      axios.post('https://hunter-todo-api.herokuapp.com/auth', { username: user})
+      .then(res => {
+        console.log(res);
+        if (res.data.token){
+          Cookies.set('token', res.data.token)
+          Cookies.set('username', user);
+          this.setState({loggedIn: true});
+          console.log("ok done logging in")
+          this.printToDoList();
+        }
+      })
+      .catch((error) => {
+        console.log(error)
+        this.setState({feedback: "That user doesn't exist"});
+      })
+    })
+    .catch((error) => {
+      console.log(error)
+      this.setState({feedback: "That user already exists"})
+    })
+    this.refs.username.value = "";
+  }
+
+
+  handleAddTask(){
+    let task = this.refs.new_task.value;
+    axios.post('https://hunter-todo-api.herokuapp.com/todo-item', {'content': task}, {headers: {'sillyauth': Cookies.get('token')}})
+    .then(res => {
+      this.printToDoList();
+    })
+    this.refs.new_task.value = "";
+  }
+
+  handleDeleteTask(task_id){
+    console.log(task_id)
+    axios.delete('https://hunter-todo-api.herokuapp.com/todo-item/'+ task_id, {headers: {'sillyauth': Cookies.get('token')}})
+    .then(res => {
+      this.printToDoList();
+    })
+  }
+
+  handleChangeStatus(task_id, status){
+    console.log(task_id)
+    console.log(status)
+    if (status == false)
+    axios.put('https://hunter-todo-api.herokuapp.com/todo-item/' + task_id, {'completed': true }, {headers: {'sillyauth': Cookies.get('token')}})
+    .then(res => {
+      this.printToDoList();
+    })
+    else
+    axios.put('https://hunter-todo-api.herokuapp.com/todo-item/' + task_id, {'completed': false }, {headers: {'sillyauth': Cookies.get('token')}})
+    .then(res => {
+      this.printToDoList();
+    })
+  }
+
+  printToDoList(){
+    axios.get('https://hunter-todo-api.herokuapp.com/todo-item', {headers: {'sillyauth': Cookies.get('token')}})
+    .then(res => {
+      this.setState({toDoList: res.data})
+      console.log(res.data)
+    })
+  }
+
   render() {
+    const { loggedIn, toDoList, feedback } = this.state;
+    //HEADER MESSAGE (NOT LOGGED IN)
     let header =
     <div>
     <h2>To-Do List App</h2>
-    <h2>Welcome! Sign In or Register!</h2>
+    <h2>Welcome! Sign in or Register!</h2>
+    <h3>{feedback}</h3>
     </div>
 
-    const { loggedIn } = this.state;
     let display;
     if (loggedIn){
-      header = <h2>{Cookies.get('username')} To-Do List</h2>
-      display = <div> <h1>you are logged in!</h1>
-      <button type = "submit" name= "submit" value = "Logout" onClick={() => this.handleLogout()}>Log out</button> </div>
-
+      header = <h1>{Cookies.get('username')} To-Do List</h1>
+      display = <div>
+      <table align="center">
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Status</th>
+            <th>Other Actions:</th>
+          </tr>
+        </thead>
+        <tbody>
+          {toDoList.map((data, i) => {
+            let completed = "Need to Do"
+            let actionButton = "Mark as Complete"
+            if (data.completed){
+              completed = "Completed"
+              actionButton = "Mark as Incomplete"
+            }
+            return(
+              <tr key={i}>
+                <td>{data.content}</td>
+                <td>{completed}</td>
+                <td>
+                  <button class="status" id="complete"type = "submit" name= "submit" value = "Status" onClick={() => this.handleChangeStatus(data.id, data.completed)}>{actionButton}</button>
+                  <br/>
+                  <button class= "status" id="delete" type = "submit" name= "submit" value = "Delete" onClick={() => this.handleDeleteTask(data.id)}>Delete</button>
+                </td>
+              </tr>
+            )
+          })}
+        </tbody>
+      </table>
+      <h2>New Task to be Completed:</h2>
+      <input class="textbox" type="text" name="new_task" placeholder="Enter new task here" ref="new_task"></input>
+      <button class="submit" type = "submit" name= "submit" value = "Submit" onClick={() => this.handleAddTask()}>Add Task</button>
+      <br/>
+      <button class="space" type = "submit" name= "submit" value = "Logout" onClick={() => this.handleLogout()}>Log out</button>
+      </div>
     }
     else{
       display =
       <div>
-        Login or Register: <br/>
-        <input type="text" name="username" placeholder="username" ref="username"></input>
-        <button type = "submit" name= "submit" value = "Login" onClick={() => this.handleLogin()}>Login</button>
-        <button type = "submit" name="submit" value= "Register">Register</button>
+        <input class="textbox" type="text" name="username" placeholder="username" ref="username"></input>
+        <br/>
+        <button class="submit" type = "submit" name= "submit" value = "Login" onClick={() => this.handleLogin()}>Login</button>
+        <button class="submit" type = "submit" name="submit" value= "Register" onClick={() => this.handleRegister()}>Register</button>
       </div>
     }
 
